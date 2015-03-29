8759769ffc8eb72f4a8cdaab1dec2c9af62c9277
David Lents <david@lents.net>
2015-03-29 02:52:02 -0700

    Update to GuzzleHttp v5, and PHP 5.4+

--- src/GuzzleZipTax.php
+++ src/GuzzleZipTax.php
@@ -28,14 +28,14 @@ class GuzzleZipTax {
          *    + Invalid postal_code, city, state return code 100, with empty results rather than the expected error code
          * @link http://www.zip-tax.com/documentation
          */
-        $this->api_response_codes = array(
+        $this->api_response_codes = [
             '100' => 'SUCCESS',
             '101' => 'INVALID_KEY',
             '102' => 'INVALID_STATE',
             '103' => 'INVALID_CITY',
             '104' => 'INVALID_POSTAL_CODE',
             '105' => 'INVALID_FORMAT'
-        );
+        ];
         $this->api_version        = 'v20';
         $this->api_url            = 'http://api.zip-tax.com';
         if (strtoupper($flavor) === 'XML') {
@@ -45,12 +45,12 @@ class GuzzleZipTax {
             $this->api_response_flavor = 'JSON';
         }
 
-        $this->api_request_params = array(
+        $this->api_request_params = [
             'key'    => $this->api_key,
             'format' => $this->api_response_flavor
-        );
+        ];
         // Init Guzzle client
-        $this->api_client = new Guzzle\Http\Client($this->api_url);
+        $this->api_client = new GuzzleHttp\Client();
     }
 
     /**
@@ -59,7 +59,7 @@ class GuzzleZipTax {
      *
      * @return array|bool
      */
-    public function fetch($zip, $optional_params = array()) {
+    public function fetch($zip, array $optional_params = [ ] ) {
         // the postalcode param is the only required param (other than the api key)
         if (empty($zip)) {
             trigger_error('A postal code parameter is required', E_USER_ERROR);
@@ -73,14 +73,23 @@ class GuzzleZipTax {
             $this->api_request_params = array_merge($this->api_request_params, $optional_params);
         }
 
-        $this->api_response_result = $this->api_client->get(
-            "/request/{$this->api_version}?",
-            array(),
-            array(
-                 'query' => $this->api_request_params
-            )
-        )->send();
+        try {
 
+            $this->api_response_result = $this->api_client->get( "{$this->api_url}/request/{$this->api_version}", [
+                    'query' => $this->api_request_params
+            ]);
+        }
+        catch ( GuzzleHttp\Exception\RequestException $e ) {
+            $msg = 'Error encountered, request: ' . $e->getRequest() . "\n";
+            if ( $e->hasResponse() ) {
+                $msg .= 'Response: ' . $e->getResponse() . "\n";
+            } else {
+                $msg .= 'Details: ' . $e->getMessage() . "\n";
+            }
+            // error_log( __FUNCTION__ . '() - Got error: ' . print_r( $e, true ) );
+            error_log('Error: ' . $msg);
+            return false;
+        }
         return $this->parseResponse();
 }
 
@@ -107,10 +116,10 @@ class GuzzleZipTax {
                     return $ret['results'];
                 }
                 else {
-                    return array(
+                    return [
                         'error'       => $this->api_response_codes[$ret['rCode']],
                         'request_uri' => $this->api_response_result->getEffectiveUrl()
-                    );
+                    ];
                 }
                 break;
         }
